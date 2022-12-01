b495ab75b1edeeb1cef75a0f122980a2573e87c4
JanMouwes <jan.mouwes@gmail.com>
2022-12-01 06:40:50 +0100

    day 1

--- src/Day_1/Name.hs
+++ src/Day_1/Name.hs
@@ -1,5 +1,8 @@
 module Day_1.Name where
 
+import Data.List.Split
+import Data.List
+
 inputFile :: String
 inputFile = "src/Day_1/input.txt"
 
@@ -15,11 +18,13 @@ run = do
   print $ part2 inputStr
 
 
-part1 :: String -> String
-part1 = undefined . parse
+part1 :: String -> Int
+part1 = maximum . map sum . parse
 
-part2 :: String -> String
-part2 = undefined . parse
+part2 :: String -> Int
+part2 = sum . take 3 . reverse . sort . map sum . parse
 
-parse :: String -> String
-parse = undefined
+parse :: String -> [[Int]]
+parse raw = 
+  let byElf = splitOn "\n\n" raw
+  in map (map read . lines) byElf
--- test/Spec/Day_1/Day_1Spec.hs
+++ test/Spec/Day_1/Day_1Spec.hs
@@ -1,7 +1,14 @@
 module Spec.Day_1.Day_1Spec where
 
+import Day_1.Name (part1, part2)
 import Test.Tasty (TestTree, testGroup)
 import Spec.Lib (createIOTests)
 
 tests :: TestTree
-tests = testGroup "Day 1" []
+tests = testGroup "Day 1" [testPart1, testPart2]
+
+testPart1 :: TestTree
+testPart1 = createIOTests "Part 1" part1 [("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000", 24000)]
+
+testPart2 :: TestTree
+testPart2 = createIOTests "Part 2" part2 [("1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000", 45000)]
